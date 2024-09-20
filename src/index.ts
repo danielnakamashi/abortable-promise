f066f8b56b0dc5079b9fec4997dbeec4923503a6
Daniel Nakamashi <dnakamashi@gmail.com>
2024-09-21 01:27:41 +0200

    feat: config typescript

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+export default class AbortablePromise<T> extends Promise<T> {
+  constructor(
+    executor: (
+      resolve: (value: T | PromiseLike<T>) => void,
+      reject: (reason?: unknown) => void,
+    ) => void,
+    options?: { signal?: AbortSignal },
+  ) {
+    let thisResolve: (value: T | PromiseLike<T>) => void;
+    let thisReject: (reason?: unknown) => void;
+
+    super(
+      (
+        resolve: (value: T | PromiseLike<T>) => void,
+        reject: (reason?: unknown) => void,
+      ) => {
+        thisResolve = resolve;
+        thisReject = reject;
+
+        options?.signal?.addEventListener('abort', () => {
+          reject(options.signal?.reason);
+        });
+      },
+    );
+
+    executor(
+      (value) => {
+        thisResolve(value);
+      },
+      (reason) => {
+        thisReject(reason);
+      },
+    );
+  }
+}
